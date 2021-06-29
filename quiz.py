6a5d9c12f93d5def14d2178b086e0b6fede6e4d3
Zac Thompson <ZThompson@stu.kerikerihigh.ac.nz>
2021-06-30 09:55:28 +1200

    fghf

--- quiz.py
+++ quiz.py
@@ -6,7 +6,7 @@ print('''
 ██ ▄▄ ██ ██    ██ ██  ███    
  ██████   ██████  ██ ███████ 
     ▀▀
-Welcome to the video game quiz!
+Welcome to the Minecraft quiz!
 ''')
 time.sleep(2)
 print("\nThis Quiz will have 20 questions the difficulty will be random the score will be out of 100%, you will also recevie a grade!")
@@ -18,3 +18,5 @@ questions_right = []
 # quiz
 Question_1 = input("What year was Minecraft Java Edition released?\nA 2014\nB 2012\nC 2011\nD 2009\nANSWER: ")
 Question_2 = input("What was Minecraft's original name?\nA Minecraft\nB Cave Game\nC Minecraft: Order of the Stone\nD Block World\nANSWER: ")
+Question_3 = input("What was Minecraft's latest update called?\nA Caves & Cliffs\nB Nether Update\nC Buzzy Bees\nD Caves and Cliffs\nANSWER: ")
+Question_4 = input("Who was Minecraft's first Dev?\n")
