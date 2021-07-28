eb2c01be45486bfa47016e50156f74df2270491b
SaltyApple05 <86578814+SaltyApple05@users.noreply.github.com>
2021-07-29 10:05:45 +1200

    Updated

--- quiz.py
+++ quiz.py
@@ -36,10 +36,15 @@ def get_response(error_message):
 
 def ask_question(question, error_message, correct):
     global score
+    global questions_right
+    global questions_wrong
     print(question)
     response = get_response(error_message)
     if response == correct:
         score += 1
+        questions_right.append(question)
+    else:
+        questions_wrong.append(question)
     
 # quiz
 
@@ -54,14 +59,14 @@ all_questions = [
     ["Who brought Minecraft/Mojang from Notch?\nA Jeb\nB Sony\nC Apple\nD Mircosoft", txt, 'D'],
     ["Which of these Minecraft mobs is the fastest?\nA Horse\nB Silverfish\nC Vindicator\nD Vex", txt, 'D'],
     ["Which of these Minecraft mobs is the slowest?\nA Cow\nB Snow golem\nC Llama\nD Polar bear", txt, 'C'],
-    ["Whats Minecrafts largest mob?\nA Ravagers\nB Ender Dragon\nC Elder Guardians", txt, 'B'],
+    ["Whats Minecrafts largest mob?\nA Ravagers\nB Ender Dragon\nC Elder Guardians\nD Sheep", txt, 'B'],
     ["Whats Minecraft smallest mob?\nA Silverfish\nB Baby turtle\nC Bat\nD Pufferfish", txt, 'B'],
     ["How many iron ingots do you need for an anvil?\nA 25\nB 31\nC 30\nD 28", txt, 'B'],
     ["What is the name for the nether verison of the slime mob?\nA Magma cube\nB Lava slime\nC Lava cube\nD Magma cream", txt, 'A'],
     ["In a furnace how many items can 1 piece of coal smelt?\nA 10\nB 12\nC 8\nD 6", txt, 'C'],
     ["What is minecrafts rarest mob?\nA Blue Axolotl\nB Zombie with Full Gold Armor\nC Left-handed baby villager chicken jockey that can pick up items, has full diamond gear, each part enchanted at level 8, an iron sword enchanted at the same level\nD Chicken", txt, 'C'],
     ["What biome do Emeralds spawn in?\nA Extreme Hills Biomes\nB Lush Caves\nC Taiga\nD Plains", txt, 'A'],
-    ["How many blocks can you fall without dying?\nA 22\nB 15\nC 10\nD 14", txt, 'A'],
+    ["How many blocks can you fall without dying?\nA 23\nB 15\nC 20\nD 14", txt, 'A'],
     ["How many biome are in minecraft?\nA 41\nB 81\nC 73\nD 100", txt, 'B'],
     ["What is the rarest biome?\nA Badlands\nB Giant Tree Taiga\nC Modified Jungle Edge\nD Mushroom Fields", txt, 'C'],
 ]
@@ -71,8 +76,22 @@ for question, error_message, correct in all_questions:
 # scoring after quiz
 
 percent = (score/20)*100
-
-print(f"You got {percent}%")
+# a = 100 ~ 90 b = 89 ~ 80 c = 79 ~ 70 d = 69 ~ 60 e = 59 ~ 50 f = 49 ~ 0
+if percent >= 90:
+   grade = "A"
+elif 80 <= percent <= 89:
+    grade = "B"
+elif 70 <= percent <= 79:
+    grade = "C"
+elif 60 <= percent <= 69:
+    grade = "D"
+elif 50 <= percent <= 59:
+    grade = "E"
+else:
+    grade = "F"
+print("You got these questions wrong!")
+print(questions_wrong)
+print(f"You got {percent}%!\nYour Grade is :{grade}")
 if percent == 100:
     print("100% WELL DONE!")
 elif percent >= 50:
